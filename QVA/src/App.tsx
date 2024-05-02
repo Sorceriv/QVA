a6c044be4fd522612401804903d3ca5ab2f684ec
sorceriv <sorceriv@gmail.com>
2024-05-02 21:07:06 +0800

    Added close open animation (to improve), transferred chat dialogues to object array for easier time in the future for API calls. Will add open close animations and draggable chatbox.

--- QVA/src/App.tsx
+++ QVA/src/App.tsx
@@ -1,15 +1,18 @@
 import ChatButton from './components/ChatButton/ChatButton';
 import ChatBox from './components/ChatBox/ChatBox';
 import './App.scss';
+import { useState } from 'react';
 
 function App() {
+  const [chatOpen, setChatOpen] = useState(false);
   const onButtonClick = (text: string) => {
     console.log(text);
+    setChatOpen(!chatOpen);
   }
   return (
     <>
       <ChatButton onClick={onButtonClick}/>
-      <ChatBox/>
+      <ChatBox opened={chatOpen}/>
     </>
   )
 }
--- QVA/src/components/ChatBox/ChatBox.tsx
+++ QVA/src/components/ChatBox/ChatBox.tsx
@@ -1,22 +1,45 @@
 import './ChatBox.scss';
 import ChatDialogue from './../ChatDialogue/ChatDialogue';
 
-function ChatBox() {
+interface Props {
+    opened: boolean,
+}
+
+function ChatBox({opened}: Props) {
+    //CORS logic happens here I guess
+    var conversation = [
+        {
+            role: "user",
+            message: "Hi",
+        },
+        {
+            role: "bot",
+            message: "Hello, I am bot.",
+        },
+        {
+            role: "user",
+            message: "I need help in fixing vulnerabilities regarding Linux systems failing to start whenever I reboot it.",
+        },
+        {
+            role: "bot",
+            message: "Sure! Let me help you with that. To do that we need to dawjidjasijdoawkdaw;kdawdwa",
+        },
+        {
+            role: "user",
+            message: "Thanks.",
+        },
+    ]
     return (
         <>
-            <div className="main-chatbox-container">
+            <div className="main-chatbox-container" style={opened ? {width: "500px", height: "500px"} : {width: "0px", height: "0px"}}>
                 <div className="top-area-container">
 
                 </div>
                 <div className="chat-area">
                     {/* <ChatDialogue /> */}
-                    <ChatDialogue role={"user"} message="HAHAHAHAHAHAHAHA"/>
-                    <ChatDialogue role={"bot"} message="HAHAHAHA"/>
-                    <ChatDialogue role={"user"} message="HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA"/>
-                    <ChatDialogue role={"user"} message="HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA"/>
-                    <ChatDialogue role={"bot"} message="HAHAHAHA"/>
-                    <ChatDialogue role={"user"} message="HAHAHAHAHAHAHAHA"/>
-                    <ChatDialogue role={"user"} message="HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA"/>
+                    {conversation.map((item, key) => (
+                        <ChatDialogue role={item.role} message={item.message}/>
+                    ))}
                 </div>
                 <div className="input-area">
 
