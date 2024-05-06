77a223c6cadf0a241379b3be85ab96b66645ca94
sorceriv <sorceriv@gmail.com>
2024-05-06 23:47:35 +0800

    Added to frontend can now chat using front-end, added comments line 67-69 chatbox.tsx on what to fix next

--- QVA/src/components/ChatBox/ChatBox.tsx
+++ QVA/src/components/ChatBox/ChatBox.tsx
@@ -2,7 +2,7 @@ import './ChatBox.scss';
 import ChatDialogue from './../ChatDialogue/ChatDialogue';
 
 import axios from "axios";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 interface Props {
     opened: boolean,
@@ -10,29 +10,31 @@ interface Props {
 
 function ChatBox({opened}: Props) {
     //CORS logic happens here I guess
-    var conversation = [
-        {
-            role: "user",
-            message: "Hi",
-        },
-        {
-            role: "bot",
-            message: "Hello, I am bot.",
-        },
-        {
-            role: "user",
-            message: "I need help in fixing vulnerabilities regarding Linux systems failing to start whenever I reboot it.",
-        },
-        {
-            role: "bot",
-            message: "Sure! Let me help you with that. To do that we need to dawjidjasijdoawkdaw;kdawdwa",
-        },
-        {
-            role: "user",
-            message: "Thanks.",
-        },
-    ];
+    // var conversation = [
+    //     {
+    //         role: "user",
+    //         message: "Hi",
+    //     },
+    //     {
+    //         role: "bot",
+    //         message: "Hello, I am bot.",
+    //     },
+    //     {
+    //         role: "user",
+    //         message: "I need help in fixing vulnerabilities regarding Linux systems failing to start whenever I reboot it.",
+    //     },
+    //     {
+    //         role: "bot",
+    //         message: "Sure! Let me help you with that. To do that we need to dawjidjasijdoawkdaw;kdawdwa",
+    //     },
+    //     {
+    //         role: "user",
+    //         message: "Thanks.",
+    //     },
+    // ];
 
+    const [conversation, setConversation] = useState([] as any);
+    const [message, setMessage] = useState({role: "bot", message:"Hi, tell me your problems on your machine."})
     // useEffect(() => {
     //     fetch("http://localhost:5000")
     //       .then((res) => res.json())
@@ -51,15 +53,24 @@ function ChatBox({opened}: Props) {
         formData.append("text", event.target["user-text"].value);
 
         //Add user chat to conversaion use useeffect for interval and typing effect
+        setConversation([...conversation, {role: "user", message: event.target["user-text"].value}])
         try {
             axios.post("http://localhost:5000/upload", formData).then((res: any) => {
                 console.log("MESSAGE: " + res.data.message);
                 //Add bot response to conversation useeffect for interval and typing effect
+                setMessage({role:"bot", message: res.data.message});
             });
         } catch (error) {
             console.log("ERROR: " + error);
         }
     }
+    //add automatic scrolldown on new chat
+    //fix empty message starter/fix errors when emptying fixed using as any but find a more readable and optimized solution
+    //Empty textbox when typing
+    
+    useEffect(() => {
+        setConversation([...conversation, message]);
+    }, [message]);
 
     return (
         <>
